a39274913870a33c2690df1c0b02b322f1168f07
Brian Connolly <Brian.connolly71@gmail.com>
2021-10-22 21:49:19 +0100

    add: added smooth scrolling behavior and removed overflow and scrollbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,8 +2,13 @@ import "./topbar.scss"
 
 export default function Topbar() {
     return (
-        <div className="topbar">
-            
+        <div className="topbar" id="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">Logo</a>
+                </div>
+                <div className="right"></div>
+            </div>
         </div>
     )
 }
