18ee9e24f79e5b1e13271597528e9b276f889055
Brian Connolly <Brian.connolly71@gmail.com>
2021-10-23 19:18:28 +0100

    add: added material icons and working on the top nav bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,20 @@
 import "./topbar.scss"
+import {Mail, PhoneIphone} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar" id="topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">Logo</a>
+                    <a href="#intro" className="logo">Brian Connolly</a>
+                    <div className="itemContainer">
+                        <PhoneIphone className="icon" />
+                        <span>(086) 3268858</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>Brian.connolly71@gmail.com</span>
+                    </div>
                 </div>
                 <div className="right"></div>
             </div>
