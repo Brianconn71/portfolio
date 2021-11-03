440d31f576b45cf9a2fffb7116441f4edcafc4a1
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-03 13:01:02 +0000

    add: added finishing touches to menu

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,12 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            hi
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="./assets/pic.jpg" alt="" />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
