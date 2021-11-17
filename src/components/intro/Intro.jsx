1578ae8ebf599da935b95719084dcf32cb1b06b7
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-17 12:52:37 +0000

    add: added intro paragraph and ityped.js

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,16 @@ export default function Intro() {
                     <img src={process.env.PUBLIC_URL + "/assets/pic.jpg"} alt="" />
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello, My name is </h2>
+                    <h1>Brian Connolly</h1>
+                    <h3>I am a Freelance <span>Web Developer</span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src={process.env.PUBLIC_URL + "/assets/chevron-down-line-svgrepo-com.svg"} alt="" />
+                </a>
+            </div>
         </div>
     )
 }
