778e97f8193143edb82c2830a55b3c98672d4466
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-18 21:43:43 +0000

    add: finished up intro section and working on portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss"
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 800,
+            backSpeed: 60,
+            strings: ["Web Designer", "Web Developer", "Content Creator"]
+        });
+    }, [])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +26,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hello, My name is </h2>
                     <h1>Brian Connolly</h1>
-                    <h3>I am a Freelance <span>Web Developer</span></h3>
+                    <h3>I am a Freelance <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src={process.env.PUBLIC_URL + "/assets/chevron-down-line-svgrepo-com.svg"} alt="" />
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src={process.env.PUBLIC_URL + "/assets/placeholder.jpg"} alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
