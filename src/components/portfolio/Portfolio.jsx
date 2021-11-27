75b07c3dc2e2c86cfbf30b5e9d4b594521afed54
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-27 16:46:21 +0000

    working on portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,50 @@
 import "./portfolio.scss"
 
 export default function Portfolio() {
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "featured",
+            title: "Featured",
+        },
+    ]
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li>Featured</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Design</li>
-                <li>Branding</li>
+                
             </ul>
             <div className="container">
+                <div className="item">
+                    <img src={process.env.PUBLIC_URL + "/assets/placeholder.jpg"} alt="" />
+                    <h3>Banking App</h3>
+                </div>
+
+                <div className="item">
+                    <img src={process.env.PUBLIC_URL + "/assets/placeholder.jpg"} alt="" />
+                    <h3>Banking App</h3>
+                </div>
+
+                <div className="item">
+                    <img src={process.env.PUBLIC_URL + "/assets/placeholder.jpg"} alt="" />
+                    <h3>Banking App</h3>
+                </div>
+
+                <div className="item">
+                    <img src={process.env.PUBLIC_URL + "/assets/placeholder.jpg"} alt="" />
+                    <h3>Banking App</h3>
+                </div>
+
+                <div className="item">
+                    <img src={process.env.PUBLIC_URL + "/assets/placeholder.jpg"} alt="" />
+                    <h3>Banking App</h3>
+                </div>
+
                 <div className="item">
                     <img src={process.env.PUBLIC_URL + "/assets/placeholder.jpg"} alt="" />
                     <h3>Banking App</h3>
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,13 @@
+import "./portfolio.scss"
+
+export default function PortfolioList() {
+    return (
+        <li className="portfolioList">
+        <li className="active">Featured</li>
+            <li>Web App</li>
+            <li>Mobile App</li>
+            <li>Design</li>
+            <li>Branding</li>
+        </li>
+    )
+}
