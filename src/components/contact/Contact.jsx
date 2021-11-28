45ca36c8992dc3e97cb4a296ab9efe8236f9cb42
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-28 15:57:46 +0000

    add: added contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import "./contact.scss"
 
 export default function Contact() {
+    const [message, setMessage] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(True)
+    }
+
     return (
         <div className="contact" id="contact">
+            <div className="left">
+                <img src={process.env.PUBLIC_URL + "/assets/handshake.svg"} alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact Me</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Enter a Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
