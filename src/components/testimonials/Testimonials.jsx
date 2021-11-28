23048e0783a29987a3fcedf93c21ec7d4de2a8a0
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-28 15:23:59 +0000

    working on testimonials section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,6 +3,23 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
+            <h1>Testimonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src={process.env.PUBLIC_URL + "/assets/arrow-left.svg"} className="left" alt="" />
+                        <img className="user" src="" alt="" />
+                        <img className="right" src="" alt="" />
+                    </div>
+                    <div className="center">
+                        wsea,dfgeasdflglasfhslaifhlaisufhloiazsfhuoiashfiluashfluiashfgluh
+                    </div>
+                    <div className="bottom">
+                        <h3>Blah</h3>
+                        <h4>Ceo of Lama</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -35,7 +35,7 @@ export default function Works() {
 
   const handleClick = (way) => {
     way === "left"
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : data.length - 1)
       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
   };
   
